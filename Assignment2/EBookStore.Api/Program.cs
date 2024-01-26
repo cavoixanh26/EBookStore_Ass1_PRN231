db6e670a236fa2482d0c17eacf084f6f97a2704f
ducNT <cavoixanh2112@gmail.com>
2024-01-26 17:13:10 +0700

    add database

--- Assignment2/EBookStore.Api/Program.cs
+++ Assignment2/EBookStore.Api/Program.cs
@@ -1,7 +1,16 @@
+using EBookStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EBookStoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
