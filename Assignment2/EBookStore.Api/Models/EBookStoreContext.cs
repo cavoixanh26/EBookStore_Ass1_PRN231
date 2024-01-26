579e84b68610c0c9e20912502f54dfd09e72de8a
ducNT <cavoixanh2112@gmail.com>
2024-01-26 17:12:31 +0700

    add models

---
+++ Assignment2/EBookStore.Api/Models/EBookStoreContext.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EBookStore.Api.Models
+{
+    public partial class EBookStoreContext : DbContext
+    {
+        public EBookStoreContext()
+        {
+        }
+
+        public EBookStoreContext(DbContextOptions<EBookStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; } = null!;
+        public virtual DbSet<Book> Books { get; set; } = null!;
+        public virtual DbSet<BookAuthor> BookAuthors { get; set; } = null!;
+        public virtual DbSet<Publisher> Publishers { get; set; } = null!;
+        public virtual DbSet<Role> Roles { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=(local); database=EBookStore;user=sa;password=123;Integrated Security=true;TrustServerCertificate=Yes");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.ToTable("Author");
+
+                entity.Property(e => e.AuthorId).HasColumnName("author_id");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("address");
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("city");
+
+                entity.Property(e => e.EmailAddress)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("email_address");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("first_name");
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("last_name");
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("phone");
+
+                entity.Property(e => e.State)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("state");
+
+                entity.Property(e => e.Zip)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("zip");
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.ToTable("Book");
+
+                entity.Property(e => e.BookId).HasColumnName("book_id");
+
+                entity.Property(e => e.Advance)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("advance");
+
+                entity.Property(e => e.Notes)
+                    .HasColumnType("text")
+                    .HasColumnName("notes");
+
+                entity.Property(e => e.Price)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("price");
+
+                entity.Property(e => e.PubId).HasColumnName("pub_id");
+
+                entity.Property(e => e.PublishedDate)
+                    .HasColumnType("date")
+                    .HasColumnName("published_date");
+
+                entity.Property(e => e.Royalty)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("royalty");
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("title");
+
+                entity.Property(e => e.Type)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("type");
+
+                entity.Property(e => e.YtdSales).HasColumnName("ytd_sales");
+
+                entity.HasOne(d => d.Pub)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.PubId)
+                    .HasConstraintName("FK__Book__pub_id__45F365D3");
+            });
+
+            modelBuilder.Entity<BookAuthor>(entity =>
+            {
+                entity.HasKey(e => new { e.AuthorId, e.BookId })
+                    .HasName("PK__BookAuth__82C1BA61FD0706B3");
+
+                entity.ToTable("BookAuthor");
+
+                entity.Property(e => e.AuthorId).HasColumnName("author_id");
+
+                entity.Property(e => e.BookId).HasColumnName("book_id");
+
+                entity.Property(e => e.AuthorOrder).HasColumnName("author_order");
+
+                entity.Property(e => e.RoyaltyPercentage)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("royalty_percentage");
+
+                entity.HasOne(d => d.Author)
+                    .WithMany(p => p.BookAuthors)
+                    .HasForeignKey(d => d.AuthorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BookAutho__autho__48CFD27E");
+
+                entity.HasOne(d => d.Book)
+                    .WithMany(p => p.BookAuthors)
+                    .HasForeignKey(d => d.BookId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BookAutho__book___49C3F6B7");
+            });
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.PubId)
+                    .HasName("PK__Publishe__2515F222BF42AD2A");
+
+                entity.ToTable("Publisher");
+
+                entity.Property(e => e.PubId).HasColumnName("pub_id");
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("city");
+
+                entity.Property(e => e.Country)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("country");
+
+                entity.Property(e => e.PublisherName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("publisher_name");
+
+                entity.Property(e => e.State)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("state");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.ToTable("Role");
+
+                entity.Property(e => e.RoleId).HasColumnName("role_id");
+
+                entity.Property(e => e.RoleDesc)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("role_desc");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.Property(e => e.EmailAddress)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("email_address");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("first_name");
+
+                entity.Property(e => e.HireDate)
+                    .HasColumnType("date")
+                    .HasColumnName("hire_date");
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("last_name");
+
+                entity.Property(e => e.MiddleName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("middle_name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.PubId).HasColumnName("pub_id");
+
+                entity.Property(e => e.RoleId).HasColumnName("role_id");
+
+                entity.Property(e => e.Source)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("source");
+
+                entity.HasOne(d => d.Pub)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.PubId)
+                    .HasConstraintName("FK__User__pub_id__412EB0B6");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.RoleId)
+                    .HasConstraintName("FK__User__role_id__403A8C7D");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
